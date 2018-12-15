a29bb46711996fe477092249c5328be6717af704
Ivan Toledo <itoledo@birdie.cl>
2018-12-14 23:48:16 -0300

    cognitive uwp

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +21,33 @@ namespace CognitiveUWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        const string key = "";
+        const string endpoint = "https://eastus.api.cognitive.microsoft.com/";
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+
+            var file = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            var features = new List<VisualFeatureTypes>()
+            {
+                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags
+            };
+
+            var VisionServiceClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key));
+            VisionServiceClient.Endpoint = endpoint;
+            using (Stream imageFileStream = await file.OpenStreamForReadAsync())
+            {
+                var analysisResult = await VisionServiceClient.AnalyzeImageInStreamAsync(imageFileStream, features);
+                var captions = string.Join(Environment.NewLine, analysisResult?.Description?.Captions?.Select(c => $"{c.Text} ({c.Confidence})") ?? Enumerable.Empty<string>());
+                await new MessageDialog(captions).ShowAsync();
+            }
         }
     }
 }
